94bb2130d9e19cc90286cbe6cc4fd5001a1145b2
Wonsang Kim <rosskysoss@gmail.com>
2024-01-20 20:28:39 +0900

    was practice 0

--- src/main/java/was/Main.java
+++ src/main/java/was/Main.java
@@ -1,7 +1,11 @@
 package was;
 
-public class Main {
-    public static void main(String[] args) {
+// GET /calculate?operand1=11&operator=+&operand2=55
+import java.io.IOException;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        new CustomWebApplicationServer(8080).start();
+        System.out.println("Aa");
     }
 }
